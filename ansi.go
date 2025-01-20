7a4771abf82b1654bd968778ed60b839b6df63e3
Isaac Forsmann <iforsmann@frontier.com>
2025-01-19 17:09:12 -0800

    more colors

--- ansi.go
+++ ansi.go
@@ -3,7 +3,16 @@ package ansi
 import "fmt"
 
 const (
-	RED Color = "255;0;0"
+	RED    Color = "255;0;0"
+	GREEN  Color = "0;255;0"
+	BLUE   Color = "0;0;255"
+	ORANGE Color = "255;128;0"
+	YELLOW Color = "255;255;0"
+	CYAN   Color = "0;255;255"
+	INDIGO Color = "127;0;255"
+	PINK   Color = "255;0;255"
+	BLACK  Color = "0;0;0"
+	WHITE  Color = "255;255;255"
 )
 
 type Color string
