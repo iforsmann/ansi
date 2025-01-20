3a4cc23404e518311a829c4e8bce77ad0905737f
Isaac Forsmann <iforsmann@frontier.com>
2025-01-19 16:25:40 -0800

    fix

--- ansi.go
+++ ansi.go
@@ -9,9 +9,9 @@ const (
 type Color string
 
 func PrintC(s string, c Color) {
-	fmt.Printf("\x1b[%sm%s\x1b[0m", c, s)
+	fmt.Printf("\x1b[38;2;%sm%s\x1b[0m", c, s)
 }
 
 func PrintLnC(s string, c Color) {
-	fmt.Printf("\x1b[%sm%s\x1b[0m\n", c, s)
+	fmt.Printf("\x1b[38;2;%sm%s\x1b[0m\n", c, s)
 }
