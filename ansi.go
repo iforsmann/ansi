c62e47c374585e6c764a4be92a6df88eae727024
Isaac Forsmann <iforsmann@frontier.com>
2025-01-19 17:01:20 -0800

    added background color support

--- ansi.go
+++ ansi.go
@@ -15,3 +15,7 @@ func PrintC(s string, c Color) {
 func PrintLnC(s string, c Color) {
 	fmt.Printf("\x1b[38;2;" + string(c) + "m" + s + "\x1b[0m\n")
 }
+
+func PrintBC(s string, c Color, b Color) {
+	fmt.Printf("\x1b[38;2;" + string(c) + "m" + "\x1b[48;2;" + string(b) + "m" + s + "\x1b[0m")
+}
